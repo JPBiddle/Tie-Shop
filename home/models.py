a94407f13651afd717854c79f64db883b0f8a063
JPBiddle <joelbiddle@hotmail.co.uk>
2024-05-28 20:00:20 +0000

    added django database with category and products

--- home/models.py
+++ home/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+# Category of product
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+# Product model
+class Product(models.Model):
+    name = models.CharField(max_length=90)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=5)
+    # Foreign key for category, enable it to be cascade deleted, set category to default first category
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    # Set description to 750 chars, enabled to be empty and empty by default
+    description = models.CharField(max_length=750, default='', blank=True, null=True)
+    colour = models.CharField(max_length=40)
+    image = models.ImageField(upload_to='uploads/product/')
+    image_2 = models.ImageField(upload_to='uploads/product/')
+
+    def __str__(self):
+        return self.name
+
