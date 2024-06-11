7ce8c4cc5ea7b8c19bc3540e4f9f3a4ca2c5a689
Joel Biddle <joelbiddle@hotmail.co.uk>
2024-06-11 19:00:45 +0000

    added relevant titles for category and colour filtering

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('<product_id>', views.product_info, name='product_info'),
     path('colour/<str:blub>', views.colour, name='colour'),
+    path('category/<str:plob>', views.category, name='category'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render, get_object_or_404
-from .models import Product, Colour
+from .models import Product, Colour, Category
 
 def all_products(request):
 
@@ -18,9 +18,19 @@ def colour(request, blub):
     try: 
         colour = Colour.objects.get(name=blub)
         products = Product.objects.filter(colour=colour)
-        return render(request, 'products/colour.html', {'products':products})
+        return render(request, 'products/colour.html', {'products':products, 'colour':colour})
 
 
     except:
         return redirect('products/products.html')
 
+def category(request, plob):
+
+    try: 
+        category = Category.objects.get(name=plob)
+        products = Product.objects.filter(category=category)
+        return render(request, 'products/category.html', {'products':products, 'category':category})
+
+
+    except:
+        return redirect('products/products.html')
