ff6791bb0621bac415d8a7c3791930ce18f750e4
Joel Biddle <joelbiddle@hotmail.co.uk>
2024-06-12 20:45:30 +0000

    began work on info pages app, created basic about us page

--- tie_shop/urls.py
+++ tie_shop/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('checkout/', include('checkout.urls')),
     path('cart/', include('cart.urls')),
+    path('info/', include('info.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
