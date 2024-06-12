b7fa8218bb642927c6adbc6f3f086a6f1819ce26
Joel Biddle <joelbiddle@hotmail.co.uk>
2024-06-12 20:51:48 +0000

    added views and urls for contact and faq

--- info/urls.py
+++ info/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('faq/', views.faq, name='faq'),
 ]
--- info/views.py
+++ info/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 def about(request):
     return render(request, 'about.html')
 
+def contact(request):
+    return render(request, 'contact.html')
 
+def faq(request):
+    return render(request, 'faq.html')
