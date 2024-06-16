708fc8939cd5e7cfaf04404ae80530ce7e9d2d12
Joel Biddle <joelbiddle@hotmail.co.uk>
2024-06-16 06:13:20 +0000

    added checkout success page

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -59,13 +59,13 @@ form.addEventListener('submit', function (ev) {
         <span class="icon" role="alert">
             <i class="fas fa-times"></i>
         </span>
-        <span>$result.error.message</span>
+        <span>${result.error.message}</span>
     `;
             $(errorDiv).html(html);
             card.update({ 'disabled': false });
             $('#submit-button').attr('disabled', false);
         } else {
-            if (result.paymentIntent.status === 'suceeded') {
+            if (result.paymentIntent.status === 'succeeded') {
                 form.submit();
             }
         }
