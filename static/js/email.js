69ff91ce924d1d2524d61afc7bf2828fc7d57d8c
Joel Biddle <joelbiddle@hotmail.co.uk>
2024-06-19 19:36:28 +0000

    added contact form and js for emailjs

---
+++ static/js/email.js
@@ -0,0 +1,12 @@
+function Contact() {
+    var tempParams = {
+        from_name:document.getElementById("from_name").value,
+        from_email:document.getElementById("from_email").value,
+        message:document.getElementById("message").value,
+    }
+
+    emailjs.send('service_xx2o104', 'template_6yopv7a', tempParams)
+    .then(function(res){
+        console.log('success', res.status);
+    })
+}
